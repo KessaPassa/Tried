c1ac85d13da88de782133ad160faadd1f417779b
KessaPassa <k.someya.gyao@gmail.com>
2017-08-07 12:03:46 +0900

    PlayerCtrlとUIManagerの分割

---
+++ Assets/PlayerCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+    private UIManager uiManager;
+	
+	void Start () {
+        uiManager = FindObjectOfType<UIManager>();
+	}
+	
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            uiManager.AppearPoseImage();
+        }
+	}
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Image poseImage;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    public void AppearPoseImage()
+    {
+        poseImage.enabled = !poseImage.enabled;
+    }
+}
